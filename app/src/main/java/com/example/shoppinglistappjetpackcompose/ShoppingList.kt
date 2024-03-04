8edf64f088d695fbe15269f944cc5104601de914
renukananda_td <renukananda2000@gmail.com>
2024-03-04 15:05:51 +0530

    Added lazy column

--- app/src/main/java/com/example/shoppinglistappjetpackcompose/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistappjetpackcompose/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.example.shoppinglistappjetpackcompose
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,9 +10,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +28,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(
@@ -53,7 +65,7 @@ fun ShoppingListApp(){
                 .padding(16.dp)
         ){
             items(sItems){
-
+                ShoppingListItem(item = it, onEditClick = {}, onDeleteClick = {})
             }
         }
     }
@@ -109,5 +121,36 @@ fun ShoppingListApp(){
                 }
             })
 
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item : ShoppingItem,
+    onEditClick : () -> Unit,
+    onDeleteClick : () -> Unit,
+){
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .border(
+                border = BorderStroke(2.dp, Color(0xFF07656F)),
+                shape = RoundedCornerShape(20)
+            )
+    ) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
+        Text(text = "Qty ${item.quantity}", modifier = Modifier.padding(8.dp))
+        Row(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            IconButton(onClick = onEditClick) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+            }
+
+            IconButton(onClick = onDeleteClick) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+            }
+        }
     }
 }
